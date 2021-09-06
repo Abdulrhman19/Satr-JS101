2988114e24797b1dff110d437cd56cf455feac68
Abdulrhman19 <am_alharthi19@hotmail.com>
2021-09-06 16:21:51 +0300

    Remove and update books

--- main.js
+++ main.js
@@ -35,13 +35,14 @@ console.log("--- Satr JS101 Bookstore Project ---");
 const choices_item = document.querySelector(".choices-items");
 const search = document.querySelector("#search");
 const buy = document.querySelector("#buy");
-const update = document.querySelector("#update");
-const add = document.querySelector("#add");
+const selection = document.querySelector("#selection");
+const addBookId = document.querySelector("#add");
 const show_choices = document.querySelector(".show-choices");
 const existence = document.getElementById("existence");
 const notifications = document.getElementById("notifications")
-const invoice = document.querySelector('.invoice')
-
+const invoice = document.querySelector('#invoice')
+const updateBookSection = document.querySelector('#updateBookSection')
+const deleteBookSection = document.querySelector('#deleteBookSection')
 
 
 document.querySelector(".choices").addEventListener("click", (e) => {
@@ -52,24 +53,26 @@ document.querySelector(".choices").addEventListener("click", (e) => {
     choices_item.classList.remove("hidden");
     show_choices.classList.add("hidden");
     search.classList.add("hidden");
+    selection.classList.add("hidden");
+    buy.classList.add("hidden");
+    addBookId.classList.add("hidden");
+    updateBookSection.classList.add('hidden')
+    deleteBookSection.classList.add('hidden')
+
     reset(existence)
     reset(notifications)
-    reset(invoice)
-    update.classList.add("hidden");
-    buy.classList.add("hidden");
-    add.classList.add("hidden");
-
+    // reset(invoice)
   } else {
     choices_item.classList.add("hidden");
     show_choices.classList.remove("hidden");
     if (requestType === "Search") {
       search.classList.remove("hidden");
     } else if (requestType === "Update") {
-      update.classList.remove("hidden");
+      selection.classList.remove("hidden");
     } else if (requestType === "Buy") {
       buy.classList.remove("hidden");
-    } else {
-      add.classList.remove("hidden");
+    } else if(requestType === 'Add') {
+      addBookId.classList.remove("hidden");
     }
   }
 });
@@ -90,7 +93,8 @@ const destroyOldRows = () => {
 
 const reset = (element) => {
   // Remove all classes from element
-  if (element.classList.length) {
+  if (element.classList.length > 0) {
+    console.log(element.classList);
     element.classList.remove(element.classList)
     element.innerText = ''
   }
@@ -146,7 +150,8 @@ const buyBook = () => {
         rowNumber = i
       }
     }
-
+    
+    // TODO: Refactoring needed here.
     if (id && quantity && balance) {
       const total = eval(`${quantity}*${record[0][3]}`)
       if (parseFloat(balance) > parseFloat(total)) {
@@ -169,6 +174,7 @@ const buyBook = () => {
     }
   })
 };
+
 
 const addNewBook = () => {
   const addBook = document.querySelector('#addBook')
@@ -199,7 +205,94 @@ const addNewBook = () => {
   })
 };
 
-const updateExistingBook = () => { };
+const updateOrDelete = () => {
+  document.querySelector('#listenForClick').addEventListener('click', () => {
+    if(document.querySelector('#updateBook').checked) {
+      updateBook()
+    } else if(document.querySelector('#deleteBook').checked) {
+      deleteBook()
+    }
+  })
+}
+
+const updateBook= () => { 
+  selection.classList.add('hidden')
+  updateBookSection.classList.remove('hidden')
+  let id
+  const updateBookBtn = document.querySelector('#updateBookBtn')
+  const updateTitle = document.getElementById("updateTitle")
+  const updateAuthor = document.getElementById("updateAuthor")
+  const updatePrice = document.getElementById("updatePrice")
+  const updateQuantity = document.getElementById("updateQuantity")
+  let record = []
+  const updateId = document.querySelector('#updateId')
+
+
+  updateId.addEventListener('input', (event) => {
+    id = event.target.value.trim()
+
+    for(let i = 0; i < booksStore.length; i++) {
+      if(booksStore[i][0] === id) {
+        record.push(booksStore[i])
+        console.log("match");
+        reset(notifications)
+        break
+      } else {
+        updateTitle.value = ''
+        updateAuthor.value = ''
+        updatePrice.value = ''
+        updateQuantity.value = ''
+        notifications.innerText = "book with this id not found"
+        notifications.classList.add('warning')
+      }
+    }
+    updateTitle.value = record[0][1]
+    updateAuthor.value = record[0][2]
+    updatePrice.value = record[0][3]
+    updateQuantity.value = record[0][4]  
+  })
+
+  updateBookBtn.addEventListener('click', () => {
+    if(updateTitle.value !== record[0][1]) {
+      record[0][1] = updateTitle.value
+    } else if(updateAuthor.value !== record[0][2]) {
+      record[0][2] = updateAuthor.value
+    } else if(updatePrice.value !== record[0][3]) {
+      record[0][3] = updatePrice.value
+    } else if(updateQuantity.value !== record[0][4]){
+      record[0][4] = updateQuantity.value
+    }
+    prepareRowsTodDisplay()
+  })
+};
+
+const deleteBook = () =>{ 
+  selection.classList.add('hidden')
+  deleteBookSection.classList.remove('hidden')
+
+  const deleteBookBtn = document.querySelector('#deleteBookBtn')
+  const bookId = document.getElementById('deleteId')
+  deleteBookBtn.addEventListener('click', () => {
+    const id = bookId.value.trim()
+    const confirmation = confirm(`Are you sure you want to delete book with id ${id}?`)
+    if(confirmation) {
+      for(let i = 0; i < booksStore.length; i++) {
+        if(booksStore[i][0] === id) {
+          booksStore.splice(booksStore[i], 1)
+          notifications.innerText = "Removed"
+          notifications.classList.add('found')
+          prepareRowsTodDisplay()
+          break
+          reset(notifications)
+        } else {
+          notifications.innerText = "Book with this id not found"
+          notifications.classList.add('warning')
+        }
+      }
+    }
+  })
+
+}
 
 const insertItemToTable = (item) => {
   const tbody = document.getElementById("row");
@@ -224,7 +317,6 @@ const createInvoice = (title, total, quantity, price) => {
 }
 
 
-// Iterate over the books
 const prepareRowsTodDisplay = () => {
   destroyOldRows()
   for (let outter = 0; outter < booksStore.length; outter++) {
